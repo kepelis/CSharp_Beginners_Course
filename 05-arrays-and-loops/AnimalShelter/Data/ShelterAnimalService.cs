e8a1854ceaa50cbe4e119d280d5f1ad579a0da3b
EgiC <EC>
2020-12-15 14:29:11 +0200

    commit all

--- 05-arrays-and-loops/AnimalShelter/Data/ShelterAnimalService.cs
+++ 05-arrays-and-loops/AnimalShelter/Data/ShelterAnimalService.cs
@@ -7,27 +7,35 @@ namespace AnimalShelter.Data
     {
         public readonly int AnimalCapacity = 20;
         private readonly ShelterCalendarService _calendarService;
-        private List<string> _animals = new List<string>();
+       // private List<string> _animals = new List<string>();
 
-        public string[] Animals { get { return _animals.ToArray(); }}
+        private List<Animal> _animals = new List<Animal>();
+        public Animal[] Animals { get { return _animals.ToArray(); }}
+
+        //public string[] Animals { get { return _animals.ToArray(); }}
 
         public ShelterAnimalService(ShelterCalendarService calendarService)
         {
             _calendarService = calendarService;
         }
 
-        public void AddAnimal(string name)
-        {
-            if (!_calendarService.IsShelterOpen())
-                return;
+        
 
-            if (_animals.Count == AnimalCapacity)
-                return;
 
-            if (_animals.Contains(name))
-                return;
+        public void AddAnimal(Animal animal)
+            {
+               if (!_calendarService.IsShelterOpen())
+                   return;
 
-            _animals.Add(name);
-        }
+              if (_animals.Count == AnimalCapacity)
+                   return;
+
+             //if (_animals.Where(x => x.Name == animal.Name).Any())
+               //     return;
+            
+             animal.DateRegistered = DateTime.Now;
+            
+             _animals.Add(animal);
+            }
     }
 }
